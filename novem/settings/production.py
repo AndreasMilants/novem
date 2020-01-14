51b0c5fd86fe9a6975ca081f00132d7f7b4c8dc0
r0668522 <andreas.milants@student.ucll.be>
2020-01-14 11:48:41 +0100

    start to production settings

--- novem/settings/production.py
+++ novem/settings/production.py
@@ -11,7 +11,11 @@ ALLOWED_HOSTS = ['0.0.0.0']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(common_settings.BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
