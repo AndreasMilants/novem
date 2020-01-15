47b3aed48d602882e25bf9c17f2fe56fe9cf0f19
r0668522 <andreas.milants@student.ucll.be>
2020-01-15 14:27:01 +0100

    bug

--- novem/settings.py
+++ novem/settings.py
@@ -5,7 +5,7 @@ import socket
 DOCKER = os.environ.get('DOCKER', default='false') == 'true'
 
 if DOCKER:
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    hostname, b, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 else:
     INTERNAL_IPS = ['127.0.0.1']
